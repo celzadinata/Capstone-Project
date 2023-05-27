afea929cdea522a15c15c1b8e5b3953c5bc01842
NaufalTQ66 <naufaltaufiq66@gmail.com>
2023-05-27 23:23:30 +0700

    Tambahan Form Jenis Produk

--- resources/views/pengusaha/produk/edit.blade.php
+++ resources/views/pengusaha/produk/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts_pengusaha.app')
-@section('title', 'Edit Produk')
+@section('title', 'Edit Supply Barang')
 @section('content')
     <style>
         .previewImage {
@@ -11,17 +11,9 @@
     <div class="content">
         <div class="container-fluid">
             <div class="card p-4">
-                <form action="{{ Route('produk.update', $produk->id) }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('produk.update', $produk->id) }}" method="POST" enctype="multipart/form-data">
                     @method('PUT')
                     @csrf
-                    <div class="form-group">
-                        <label for="jenis">Jenis</label>
-                        <select name="jenis" id="jenis" class="form-control" required>
-                            <option value="paket_usaha" {{ $produk->jenis == 'paket_usaha' ? 'selected' : '' }}>Paket Usaha
-                            </option>
-                            <option value="supply" {{ $produk->jenis == 'supply' ? 'selected' : '' }}>Supply</option>
-                        </select>
-                    </div>
                     <div class="form-group">
                         <label for="nama_produk">Nama Produk</label>
                         <input type="text" name="nama_produk" id="nama_produk" class="form-control"
@@ -42,18 +34,6 @@
                             required>
                     </div>
                     <div class="form-group">
-                        <label for="status">Status</label>
-                        <select name="status" id="status" class="form-control" required>
-                            <option value="0" {{ $produk->status == 0 ? 'selected' : '' }}>Tidak Aktif</option>
-                            <option value="1" {{ $produk->status == 1 ? 'selected' : '' }}>Aktif</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="rate">Rate</label>
-                        <input type="number" name="rate" id="rate" class="form-control" value="{{ $produk->rate }}"
-                            required>
-                    </div>
-                    <div class="form-group">
                         <label for="kategoris_id">Kategori</label>
                         <select name="kategoris_id" id="kategoris_id" class="form-control" required>
                             @foreach ($kategoris as $kategori)
@@ -63,45 +43,29 @@
                             @endforeach
                         </select>
                     </div>
-                    <div class="form-group">
-                        <label for="foto">Foto</label>
-                        <div>
-                            <input type="file" name="foto" id="foto" class="form-control-file">
-                            <button type="button" id="chooseBtn">Choose File</button>
-                        </div>
-                        <div id="previewContainer">
-                            <img src="{{ $produk->foto }}" class="previewImage">
-                        </div>
+                    <div class="mb-3">
+                        <label for="foto" class="form-label">Foto</label>
+                        <input class="form-control chooseBtn" type="file" id="foto" onchange="previewFoto(event)"
+                            required>
                     </div>
-                    <button type="submit" class="btn btn-primary">Update</button>
-                    <a href="{{ route('produk.pengusaha') }}" class="btn btn-primary">Kembali</a>
+                    <div id="previewContainer"><img src="{{ $produk->foto }}" class="previewImage"></div>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <a href="#" class="btn btn-primary">Kembali</a>
                 </form>
             </div>
         </div>
     </div>
     <script>
-        document.getElementById('chooseBtn').addEventListener('click', function() {
-            document.getElementById('foto').click();
-        });
-
-        document.getElementById('foto').addEventListener('change', function(event) {
-            var files = event.target.files;
-            var previewContainer = document.getElementById('previewContainer');
-            previewContainer.innerHTML = '';
-
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
+        function previewFoto(event) {
+            var input = event.target;
+            if (input.files && input.files[0]) {
                 var reader = new FileReader();
-
-                reader.onload = function() {
-                    var img = document.createElement('img');
-                    img.src = reader.result;
-                    img.classList.add('previewImage');
-                    previewContainer.appendChild(img);
-                }
-
-                reader.readAsDataURL(file);
+                reader.onload = function(e) {
+                    var previewContainer = document.getElementById('previewContainer');
+                    previewContainer.innerHTML = '<img src="' + e.target.result + '" class="previewImage">';
+                };
+                reader.readAsDataURL(input.files[0]);
             }
-        });
+        }
     </script>
 @endsection
