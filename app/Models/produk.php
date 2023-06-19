b4e75ac9385b1063fa85248c23fdc733c9687644
RizalEka321 <76163135+RizalEka321@users.noreply.github.com>
2023-06-19 15:33:46 +0700

    Update produk.php

--- app/Models/produk.php
+++ app/Models/produk.php
@@ -96,7 +96,7 @@ class produk extends Model
 
     public function review()
     {
-        return $this->hasMany(Review::class, 'produks_id');
+        return $this->hasMany(review::class, 'produks_id');
     }
 
     public function notif()
